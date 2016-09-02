2d24c0ed185bf4a63d5af28131b5a768ef1c79a7
cheungj8394 <CheungJ8394@sduhsd.lan>
2016-09-02 13:31:00 -0700

    added files

---
+++ APLesson_01/Lab1.java
@@ -0,0 +1,13 @@
+public class Lab1
+{
+	public static void main(String[]args)
+	{
+		System.out.println(",___,");
+		System.out.println("(O.o)");
+		System.out.println("/),,,,)");
+		System.out.println("  \"\"");
+		System.out.println("(\\___/)");
+		System.out.println("(='.'=)");
+		System.out.println("(\")_(\")");
+	}
+}
