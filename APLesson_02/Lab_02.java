21ebbe855b57ffc2a019187c1b7da2dd053becc8
cheungj8394 <jingcheung29@gmail.com>
2016-09-09 13:07:31 -0700

    Add files via upload

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,22 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int x=17;
+		int y=200;
+		int product=x*y;
+		System.out.println("17 multiplied by 200 equals"+" "+product);
+		System.out.println(" ");
+		int add=621;
+		int zip=92014;
+		System.out.println("Jing Cheung");
+		System.out.println(add+" "+"Marine View Ave");
+		System.out.println("Del Mar, CA"+" "+zip);
+		System.out.println(" ");
+		int length=33;
+		int height=44;
+		int width=55;
+		int sArea=length*height*width;
+		System.out.println("The surface area of your rectangle is"+" "+sArea);
+	}
+}
