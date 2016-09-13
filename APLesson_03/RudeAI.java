6a99216c63ef62cc1072f360a45445a544f10e3b
Jing Cheung <jingcheung29@gmail.com>
2016-09-13 13:48:23 -0700

    finished rudeai

--- APLesson_03/RudeAI.java
+++ APLesson_03/RudeAI.java
@@ -16,7 +16,25 @@ public class RudeAI
 		System.out.println("Whoa there, dinosaur. "+age+"?!! You're ancient!");
 		
 		System.out.println("What do you do for fun, "+ nam+"?");
-		String stuf=keyboard.nextLine();
+		String stuf=keyboard.next();
 		System.out.println(stuf+"? Wow you have no life you loser.");
+		
+		System.out.println("What kind of music do you listen to?");
+		String mus=keyboard.next();
+		System.out.println("Eww. Only scrubs listen to"+" "+mus+".");
+		
+		System.out.println("How many siblings do you have?");
+		int num=keyboard.nextInt();
+		System.out.println(num+"? I really hope they look better than you do.");
+		
+		System.out.println("What do you want to be when you grow up?");
+		String car=keyboard.next();
+		System.out.println("I'm pretty sure you're too stupid to be a "+car+".");
+		
+		System.out.println("So "+nam+", you're "+age+"...");
+		System.out.println("You like to "+stuf+" and listen to "+mus+"...");
+		System.out.println("Best of luck becoming a "+car+".");
+		System.out.println("I'm just playing with you "+nam+" ;)");
+		
 	}
 }
