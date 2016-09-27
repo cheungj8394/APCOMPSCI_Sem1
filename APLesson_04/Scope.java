0087157a7ca3c815a5ecbc5f5a9c9b408e4ce86c
Jing Cheung <jingcheung29@gmail.com>
2016-09-27 14:21:12 -0700

    scope,subwoofer,sum,retmethods

---
+++ APLesson_04/Scope.java
@@ -0,0 +1,14 @@
+public class Scope
+{
+	public static void main(String[]args)
+	{
+		int var2=47;
+		//System.out.println(var2);
+		mathod1();
+	}
+	public static void method1()
+	{
+		double var3=89.1;
+		System.out.println(var3);
+	}
+}
--- APLesson_04/Subwoofer.java
+++ APLesson_04/Subwoofer.java
@@ -3,18 +3,20 @@ public class Subwoofer
 {
 	public static void main(String[]args)
 	{
-		Subwoofer calc=new Subwoofer();
-		System.out.println("Enter the height:");
+		Subwoofer Vol=new Subwoofer();
+		System.out.println("Enter the height in inches:");
 		Scanner kb=new Scanner(System.in);
 		Double height=kb.nextDouble();
 		
-		System.out.println("Enter the length:");
+		System.out.println("Enter the length in inches:");
 		Double length=kb.nextDouble();
 		
-		System.out.println("Enter the width:");
+		System.out.println("Enter the width in inches:");
 		Double width=kb.nextDouble();
 		
-		System.out.println("The volume of the Subwoofer box is %f",calc.calcVol(height,length,width));
+		Double fvol=Vol.calcVol(height,length,width)/1728;
+		
+		System.out.printf("The volume of the Subwoofer box is %.2f cubic feet",fvol);
 	}
 	public double calcVol(double h, double l, double w)
 	{
