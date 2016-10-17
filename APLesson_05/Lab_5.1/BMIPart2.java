04f43239f9cfa6a545d125b66a313e7ba5b4de21
Jing Cheung <jingcheung29@gmail.com>
2016-10-17 14:33:14 -0700

    bmi

--- APLesson_05/Lab_5.1/BMIPart2.java
+++ APLesson_05/Lab_5.1/BMIPart2.java
@@ -10,20 +10,21 @@ public class BMIPart2
 		double weight=kb.nextDouble();
 		double bmi=weight*703/(height*height);
 		System.out.println("Your BMI is: "+bmi);
-		System.out.println
+		String condition=calcCondish(bmi);
+		System.out.println("You are "+condition);
 	}
-	public static void calcCondish(double bmi)
+	public static String calcCondish(double bmi)
 	{
 		String condition="";
 		if (bmi<18.5)
 			condition="Underweight";
 		if (18.5<=bmi<=24.9)
 			condition="Normal";
-		if (25<=bmi<=29.9)
+		if (25.0<=bmi<=29.9)
 			condition="Normal";
-		if (30<=bmi<=34.9)
+		if (30.0<=bmi<=34.9)
 			condition="Obese";
-		if (35<=bmi<=39.9)
+		if (35.0<=bmi<=39.9)
 			condition="Very Obese";
 		if (bmi>39.9)
 			condition="Morbidly Obese";
