dcb5394edf9742be19eba5e67704dd7e2e3b0f26
Jing Cheung <jingcheung29@gmail.com>
2016-11-04 14:33:10 -0700

    digitadder and lesson 7

---
+++ APLesson_07/DigitAdder.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class DigitAdder
+{
+	static int num;
+	static int sum=0;
+	public static void main(String[]args)
+	{
+		Scanner kb= new Scanner(System.in);
+		System.out.println("Enter a number:");
+		num=kb.nextInt();
+		
+	}
+	public static void sumDigits()
+	{
+		num=
+		while(num>0)
+		{
+			
+		}
+	}
+}
