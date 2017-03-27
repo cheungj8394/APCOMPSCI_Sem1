5415a1b23da2e21e29d6bff1c2f09574663b8e2d
Jing Cheung <jingcheung29@gmail.com>
2017-03-27 14:31:06 -0700

    dabbb dum

--- APLesson_14/Lab_14.1/Satellite2.java
+++ APLesson_14/Lab_14.1/Satellite2.java
@@ -13,27 +13,30 @@ public class Satellite2
        double[] home = {0, 0};
 
 
-       String printout = "\n\n" +
-               "==========================" + "\nStarting locations...";
-
-
+      System.out.println("\n\n" + "==========================" + "\nStarting locations...\n");
        for (Location2 l : locate)
        {
-           printout += "\nLocation for " + l.getID() + ": (" + getLocation(l.getLoc()) + ")";
+           System.out.println("Location for " + l.getID() + ": (" + getLocation(l.getLoc()) + ")");
        }
 
-
-       printout += "\n\n" + "==========================" +
-                   "\nDistance from home...";
-
-
-       for (Location2 1 : locate)
+	   System.out.println("\n\n" + "==========================");
+	   for (Location2 l: locate)
+	   {
+		   double x, y;
+		   x = (Math.random()*100)+1;
+		   y = (Math.random()*100)+1;
+		   System.out.printf("After %6d moved (%4.2f, %4.2f)\n", l.getID(), x ,y);
+		   locate.move(x,y);
+		   System.out.println("New Location: (" + getLocation(l.getLoc()) + ")\n");
+	   }
+	   
+       System.out.println("\n" + "==========================" + "\nDistance from home...\n");
+	   
+       for (Location2 l : locate)
        {
-           printout += "\nDistance for " + l.getID() + ": (" + getDistance(l.getLoc(), home)+ ")";
+           System.out.printf("Distance for %6d: (%4.2f)\n", l.getID(), getDistance(l.getLoc(), home));
        }
 
-
-       System.out.println(printout);
    }
 
 
@@ -45,6 +48,6 @@ public class Satellite2
 
    public static String getLocation(double[] loc)
    {
-       return loc[0] + ", " + loc[1];
+       return String.format("%4.2f, %4.2f", loc[0], loc[1]);
    }
 }
