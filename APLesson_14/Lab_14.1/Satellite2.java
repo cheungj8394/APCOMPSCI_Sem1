ddf6585947ecea35939165cc4179c7dddd04ea03
Jing Cheung <jingcheung29@gmail.com>
2017-04-19 14:10:12 -0700

    finished labs 14

--- APLesson_14/Lab_14.1/Satellite2.java
+++ APLesson_14/Lab_14.1/Satellite2.java
@@ -13,25 +13,21 @@ public class Satellite2
        double[] home = {0, 0};
 
 
-      System.out.println("\n\n" + "==========================" + "\nStarting locations...\n");
+       String printout = "\n\n" +
+               "==========================" + "\nStarting locations...";
+
+
        for (Location2 l : locate)
        {
-           System.out.println("Location for " + l.getID() + ": (" + getLocation(l.getLoc()) + ")");
-       }
-
-	   System.out.println("\n\n" + "==========================");
-	   for (Location2 l: locate)
-	   {
 		   double x, y;
 		   x = (Math.random()*100)+1;
 		   y = (Math.random()*100)+1;
 		   System.out.printf("After %6d moved (%4.2f, %4.2f)\n", l.getID(), x ,y);
-		   locate.move(x,y);
 		   System.out.println("New Location: (" + getLocation(l.getLoc()) + ")\n");
+		   ((Car)l).move(x, y);
 	   }
 	   
        System.out.println("\n" + "==========================" + "\nDistance from home...\n");
-	   
        for (Location2 l : locate)
        {
            System.out.printf("Distance for %6d: (%4.2f)\n", l.getID(), getDistance(l.getLoc(), home));
