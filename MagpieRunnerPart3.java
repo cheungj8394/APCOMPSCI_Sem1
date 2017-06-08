90fad383b9b1fd00fbe241ccf1adb5d33db92a88
cheungj8394 <jingcheung29@gmail.com>
2017-06-07 21:24:13 -0700

    Update MagpieRunnerPart3.java

--- MagpieRunnerPart3.java
+++ MagpieRunnerPart3.java
@@ -1,4 +1,11 @@
-
+import java.util.Scanner; 
+ 
+ 
+ /** 
+  * A simple class to run the Magpie class. 
+  * @author Laurie White 
+  * @version April 2012 
+  */
 public class MagpieRunnerPart3 
 {
     /** 
